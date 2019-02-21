b1412c4c13e0f29c690e740c80561ce3c5b9daae
adr105 <adriac01@ucm.es>
2019-02-21 16:25:01 +0100

    Game init and unsort function

--- src/practica1.js
+++ src/practica1.js
@@ -21,7 +21,33 @@ var couples = new Array(MAX_CARDS/2);
 MemoryGame.prototype = {
 
 	initGame: function() {
-		
+		/*Declaramos todas las cartas*/
+		this.cards[0] = new MemoryGameCard("8-ball");
+		this.cards[1] = new MemoryGameCard("8-ball");
+		this.cards[2] = new MemoryGameCard("potato");
+		this.cards[3] = new MemoryGameCard("potato");
+		this.cards[4] = new MemoryGameCard("dinosaur");
+		this.cards[5] = new MemoryGameCard("dinosaur");
+		this.cards[6] = new MemoryGameCard("kronos");
+		this.cards[7] = new MemoryGameCard("kronos");
+		this.cards[8] = new MemoryGameCard("rocket");
+		this.cards[9] = new MemoryGameCard("rocket");
+		this.cards[10] = new MemoryGameCard("unicorn");
+		this.cards[11] = new MemoryGameCard("unicorn");
+		this.cards[12] = new MemoryGameCard("guy");
+		this.cards[13] = new MemoryGameCard("guy");
+		this.cards[14] = new MemoryGameCard("zeppelin");
+		this.cards[15] = new MemoryGameCard("zeppelin");
+
+
+		/*Las desordenamos
+			fuente: https://es.stackoverflow.com/questions/21097/desordenar-array-y-a%C3%B1adir-datos-a-otro-array
+		*/
+
+		unsort(this.cards);
+	
+
+
 	}
 
 	draw: function() {
@@ -92,5 +118,21 @@ MemoryGameCard.prototype = {
 	}
 		
 
+}
+
+function unsort(deck){
+
+	var desordenado = new Array(MAX_CARDS);
+	while(true){
+		if(!deck.length){break}
+		var sacado = deck.shift();
+		var aleatorio=Math.floor(Math.random()*(desordenado.length+1));
+		var inicio = desordenado.slice(0,aleatorio);
+        var medio = sacado;
+        var fin = desordenado.slice(aleatorio,desordenado.length);
+        desordenado = (inicio).concat(medio).concat(fin);
+
+	}
+return desordenado;
 }
 
